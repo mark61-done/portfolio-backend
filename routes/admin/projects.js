f4c24a6d0c884d71b1698b31f0ae37f9331fe36c
mark61-done <markoolando558@gmail.com>
2026-01-19 16:56:11 +0300

    fixed

--- routes/admin/projects.js
+++ routes/admin/projects.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Project = require('../../models/Project');
+const Project = require('../../models/project');
 const multer = require('multer');
 const path = require('path');
 const fs = require('fs');
